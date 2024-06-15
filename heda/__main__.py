499207e70235ffdeae253984d4ff739ec7156486
Suphi <suphiozturk8@gmail.com>
2024-06-15 07:29:28 +0300

    Update __main__.py

--- heda/__main__.py
+++ heda/__main__.py
@@ -10,7 +10,7 @@ async def main():
     log(__name__).info("Bot starting...")
     await heda.start()
     log(__name__).info("Bot started.")
-    print("Hi")
+    print("mrb")
     await heroku()
     await idle()
     log(__name__).info("Bot stopping...")
