9f7e6a7372b32fc9f5137ea7e208a55f2d71940f
Suphi <suphiozturk8@gmail.com>
2024-06-17 19:24:21 +0300

    Update heroku_tool.py

--- heda/plugins/heroku_tool.py
+++ heda/plugins/heroku_tool.py
@@ -2,6 +2,7 @@
 import os
 import shutil
 import socket
+import heroku3
 import asyncio
 import urllib3
 from datetime import datetime
@@ -15,12 +16,41 @@ from pyrogram.types import Message
 
 from heda import paste, log
 from heda.config import HerokuConfig, LogConfig, DataConfig
-from heda.utils.heroku import HEROKU_APP, is_heroku
 
 
 urllib3.disable_warnings(
     urllib3.exceptions.InsecureRequestWarning
 )
+
+HEROKU_APP = None
+
+def is_heroku():
+    log(__name__).info(socket.getfqdn())
+    return "heroku" in socket.getfqdn()
+
+
+def heroku():
+    global HEROKU_APP
+    if is_heroku():
+        if HerokuConfig.HEROKU_API_KEY and HerokuConfig.HEROKU_APP_NAME:
+            try:
+                Heroku = heroku3.from_key(
+                    HerokuConfig.HEROKU_API_KEY
+                )
+                HEROKU_APP = Heroku.app(
+                    HerokuConfig.HEROKU_APP_NAME
+                )
+                log(__name__).info(
+                    "Heroku App configured."
+                )
+            except Exception as e:
+                log(__name__).error(str(e))
+        else:
+            log(__name__).warning(
+                "Please make sure your Heroku API Key and Your App name are configured correctly in the heroku."
+            )
+
+heroku()
 
 
 @Client.on_message(
