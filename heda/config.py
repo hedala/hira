66dcad78820e48ad46876b74377252e0e6e2fd50
Suphi <suphiozturk8@gmail.com>
2024-06-18 09:04:04 +0300

    Update config.py

--- heda/config.py
+++ heda/config.py
@@ -4,37 +4,6 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 
-vars_ = {
-    "BOT_NAME": "str",
-    "API_ID": "int",
-    "API_HASH": "str",
-    "BOT_TOKEN": "str",
-    "DEBUG": "bool",
-    "LOG_TO_FILE": "bool",
-    "LOG_FILE_PATH": "Path",
-    "LOG_CHAT_ID": "int",
-    "REDIS_URL": "str",
-    "REDIS_PASSWORD": "str",
-    "OWNER_ID": "int",
-    "HEROKU_API_KEY": "str",
-    "HEROKU_APP_NAME": "str",
-    "UPSTREAM_BRANCH": "str",
-    "UPSTREAM_REPO": "str",
-    "GIT_TOKEN": "str"
-}
-
-
-def make_config():
-    with open("config.env", "a") as file:
-        for var in vars_:
-            type_ = vars_[var]
-            inp = input(f"Enter {var} ({type_})\n: ")
-            file.write(f'{var}="{inp}"\n')
-    from heda import log
-    log(__name__).info(
-        "Successfully created config.env file..."
-    )
-
 def strtobool(val):
     val = val.lower()
     if val in ("y", "yes", "t", "true", "on", "1"):
@@ -44,13 +13,7 @@ def strtobool(val):
     else:
         raise ValueError("invalid truth value %r" % val)
 
-"""
-if os.path.exists("config.env"):
-    load_dotenv("config.env")
-else:
-    make_config()
-    load_dotenv("config.env")
-"""
+
 load_dotenv()
 
 
