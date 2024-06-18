406645c643ce86104566030d1844330b655d1171
heda <103752316+hedala@users.noreply.github.com>
2024-06-18 11:23:46 +0300

    Update ping.py

--- heda/plugins/ping.py
+++ heda/plugins/ping.py
@@ -11,7 +11,7 @@ async def handle_ping_command(client: Client, message: Message):
         sent_message = await message.reply_text("Pong!")
         end_time = time.time()
         ping_time = (end_time - start_time) * 1000
-        await sent_message.edit_text(f"Pong! {int(ping_time)} ms")
+        await sent_message.edit_text(f"`Pong! {int(ping_time)} ms`")
         log(__name__).info(
             f"{message.command[0]} command was called by {message.from_user.full_name} with ping time {int(ping_time)} ms."
         )
@@ -25,15 +25,15 @@ async def handle_id_command(_, message: Message):
             user_id = message.reply_to_message.from_user.id
             group_id = message.chat.id
             await message.reply_text(
-                f"Alıntılanan Kişinin ID'si: {user_id}\n"
-                f"Grup ID'si: {group_id}"
+                f"Alıntılanan Kişinin ID'si: `{user_id}`\n"
+                f"Grup ID'si: `{group_id}`"
             )
         else:
             user_id = message.from_user.id
             group_id = message.chat.id
             await message.reply_text(
-                f"Sizin ID'niz: {user_id}\n"
-                f"Grup ID'si: {group_id}"
+                f"Sizin ID'niz: `{user_id}`\n"
+                f"Grup ID'si: `{group_id}`"
             )
         log(__name__).info(
             f"{message.command[0]} command was called by {message.from_user.full_name}."
