317d7ee8ba4dc5c55791286c49d640f24f749cd2
heda <astroheda@gmail.com>
2024-06-27 02:01:36 +0300

    Update zip.py

--- heda/plugins/zip.py
+++ heda/plugins/zip.py
@@ -1,7 +1,7 @@
 import os
 import zipfile
 import tempfile
-import subprocess
+import libarchive.public
 from pyrogram import Client, filters
 
 @Client.on_message(filters.command("unzip") & filters.private)
@@ -28,17 +28,25 @@ async def unzip_file(client, message):
                 with zipfile.ZipFile(zip_path, 'r') as zip_ref:
                     zip_ref.extractall(temp_dir)
             elif file_name.endswith(".zip.001") or file_name.endswith(".7z"):
-                # 7-Zip komut satırı aracını kullanarak dosyayı çıkar
-                subprocess.run(['7z', 'x', zip_path, f'-o{temp_dir}'], check=True)
+                # libarchive kullanarak dosyayı çıkar
+                with libarchive.public.file_reader(zip_path) as e:
+                    for entry in e:
+                        with open(os.path.join(temp_dir, entry.pathname), 'wb') as f:
+                            for block in entry.get_blocks():
+                                f.write(block)
 
             # Çıkarılan dosyaları kullanıcıya gönder
             for root, dirs, files in os.walk(temp_dir):
                 for file in files:
                     file_path = os.path.join(root, file)
-                    await client.send_document(message.chat.id, file_path)
+                    if os.path.getsize(file_path) > 0:
+                        await client.send_document(message.chat.id, file_path)
+                    else:
+                        await message.reply(f"{file} dosyası boş olduğu için gönderilemiyor.")
 
-        except (zipfile.BadZipFile, subprocess.CalledProcessError):
+        except (zipfile.BadZipFile, libarchive.exception.ArchiveError):
             await message.reply("Geçersiz bir dosya.")
         finally:
             # İndirilen zip dosyasını sil
             os.remove(zip_path)
+            
