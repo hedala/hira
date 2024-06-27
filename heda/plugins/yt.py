df9ad1ad74b735b3df992ee9f79d34a5c76f8730
heda <astroheda@gmail.com>
2024-06-27 20:30:34 +0300

    Update yt.py

--- heda/plugins/yt.py
+++ heda/plugins/yt.py
@@ -1,41 +1,54 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import yt_dlp
+import os
 
+# Function to get video qualities
 def get_video_qualities(url):
     ydl_opts = {
-        'format': 'bestvideo',
+        'format': 'bestvideo+bestaudio/best',
         'noplaylist': True,
         'quiet': True
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info_dict = ydl.extract_info(url, download=False)
         formats = info_dict.get('formats', [])
-        quality_buttons = []
+        qualities = {}
         for f in formats:
-            # Check if 'height' key exists and is one of the desired resolutions
-            if 'height' in f and f['height'] in [2160, 1440, 1080, 720]:
-                button = InlineKeyboardButton(f"{f['height']}p", callback_data=f"{f['format_id']}")
-                quality_buttons.append(button)
-        return quality_buttons
+            if f.get('vcodec') != 'none' and f.get('acodec') != 'none':
+                quality = f.get('format_note')
+                if quality in ['2160p', '1440p', '1080p', '720p']:
+                    qualities[quality] = f.get('format_id')
+        return qualities
 
-@Client.on_message(filters.command("yt") & filters.private)
-def youtube_command(client, message):
-    url = message.text.split(' ')[1]
-    quality_buttons = get_video_qualities(url)
-    reply_markup = InlineKeyboardMarkup([quality_buttons])
-    message.reply_text("Choose the quality:", reply_markup=reply_markup)
+@Client.on_message(filters.command("yt"))
+async def yt_command(client, message):
+    url = message.text.split(" ", 1)[1]
+    qualities = get_video_qualities(url)
+    buttons = [
+        [InlineKeyboardButton(quality, callback_data=f"{url}|{format_id}")]
+        for quality, format_id in qualities.items()
+    ]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await message.reply("Choose the quality:", reply_markup=reply_markup)
 
-@Client.on_callback_query()
-def answer(client, callback_query):
-    format_id = callback_query.data
-    url = callback_query.message.reply_to_message.text.split(' ')[1]
+# Function to download video
+def download_video(url, format_id):
     ydl_opts = {
-        'format': f'{format_id}+bestaudio',
-        'outtmpl': '%(title)s.%(ext)s',
+        'format': format_id,
+        'outtmpl': 'downloads/%(title)s.%(ext)s',
+        'quiet': True
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-        file_name = ydl.prepare_filename(ydl.extract_info(url))
-        callback_query.message.reply_video(video=file_name)
-        callback_query.answer("Downloading and sending your video!")
+        info_dict = ydl.extract_info(url, download=True)
+        return ydl.prepare_filename(info_dict)
+
+@Client.on_callback_query()
+async def callback_query_handler(client, callback_query):
+    data = callback_query.data.split("|")
+    url, format_id = data[0], data[1]
+    await callback_query.message.edit_text("Downloading...")
+    video_path = download_video(url, format_id)
+    await client.send_video(callback_query.message.chat.id, video=video_path)
+    os.remove(video_path)
+    
