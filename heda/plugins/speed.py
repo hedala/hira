82788e1b0f7fc6b9c2f05bcdfa343779150d8c82
heda <astroheda@gmail.com>
2024-07-03 13:23:48 +0300

    Update speed.py

--- heda/plugins/speed.py
+++ heda/plugins/speed.py
@@ -1,9 +1,10 @@
 from pyrogram import Client, filters
 import speedtest
+import asyncio
 
-@Client.on_message(filters.command("hız"))
-def speedtest_command(client, message):
+@Client.on_message(filters.command("dl"))
+async def speedtest_command(client, message):
     speed = speedtest.Speedtest()
-    download_speed = speed.download()
-    upload_speed = speed.upload()
-    message.reply_text(f"İndirme Hızı: {download_speed / 1024 / 1024:.2f} Mbps\nYükleme Hızı: {upload_speed / 1024 / 1024:.2f} Mbps")
+    download_speed = await asyncio.get_event_loop().run_in_executor(None, speed.download)
+    upload_speed = await asyncio.get_event_loop().run_in_executor(None, speed.upload)
+    await message.reply_text(f"İndirme Hızı: {download_speed / 1024 / 1024:.2f} Mbps\nYükleme Hızı: {upload_speed / 1024 / 1024:.2f} Mbps")
