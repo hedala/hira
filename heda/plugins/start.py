ce1add3b331566237d0b701ba323adca00fd8a33
heda <astroheda@gmail.com>
2024-07-12 09:48:37 +0300

    Create start.py

---
+++ heda/plugins/start.py
@@ -0,0 +1,30 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from heda import redis, log
+
+@Client.on_message(filters.command(["start"]))
+async def handle_start_command(client: Client, message: Message):
+    try:
+        user_id = message.from_user.id
+        start_message = (
+            f"Merhaba! {message.from_user.mention}\n"
+        )
+        await message.reply_text(
+            text=start_message,
+            quote=True
+        )
+    
+        log(__name__).info(
+            f"{message.command[0]} command was called by {message.from_user.full_name}."
+        )
+
+        new_user = await redis.is_added(
+            "NEW_USER", user_id
+        )
+        if not new_user:
+            await redis.add_to_db(
+                "NEW_USER", user_id
+            )
+
+    except Exception as e:
+        log(__name__).error(f"Error: {str(e)}")
